7ff40bfd4d640538bac023ed8eb80ddea9c37935
Reza <reza.shahnazar@gmail.com>
2021-08-21 17:39:14 +0430

    second commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return "<h1>Home</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
