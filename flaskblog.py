ec0c1487bb4c9886d212e1847bd9c9086fa535d6
Reza <reza.shahnazar@gmail.com>
2021-08-21 17:51:11 +0430

    templates added

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1>Home</h1>"
+    return render_template('home.html')
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
